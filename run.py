f4f7f3dc7bc1d56d90be90d62e47203184907c69
Ben Evans <ben.evans_9@hotmail.com>
2018-10-27 17:12:18 +1300

    Refactoring

--- run.py
+++ run.py
@@ -1,9 +1,9 @@
 from helpers import read_data
-from src.base import EvolutionaryBase
+from src.forest import EvolutionaryForest
 
 data_x, data_y = read_data("data/balloons.data")
 
 print(data_x)
 
-forest = EvolutionaryBase()
+forest = EvolutionaryForest()
 forest.fit(data_x, data_y)
--- src/base.py
+++ src/base.py
@@ -17,7 +17,7 @@ mask_type = type("Mask", (np.ndarray, ), {})
 
 class EvolutionaryBase(Classifier):
 
-    def __init__(self, max_trees=10, max_depth=10, num_generations=50):
+    def __init__(self, max_trees, max_depth, num_generations):
         self.max_trees = max_trees
         self.max_depth = max_depth
         self.num_generations = num_generations
@@ -30,41 +30,6 @@ class EvolutionaryBase(Classifier):
 
     def _reset_pset(self):
         self.pset = gp.PrimitiveSetTyped("MAIN", [mask_type, train_data_type], train_data_type)
-
-    def _get_majority_class(self, labels):
-        classes, class_counts = np.unique(labels, return_counts=True)
-        class_probabilities = class_counts / np.sum(class_counts)  # Normalize the counts
-
-        majority_class_idx = np.argmax(class_probabilities)
-        return classes[majority_class_idx]
-
-    def _fitness_function(self, individual, train_data):
-        callable_tree = self.toolbox.compile(expr=individual)
-
-        training_data, valid_data = train_test_split(train_data, test_size=0.2)
-
-        majority_class = self._get_majority_class(training_data[:, -1])
-
-        predictions = []
-
-        for mask in valid_data:
-
-            # The subset of data that matches our mask
-            matching_data = callable_tree(mask, training_data)
-
-            if matching_data is None or matching_data.shape[0] == 0:
-                # Predict majority class, this means either the tree is bad or we have never seen an instance like this
-                prediction = majority_class
-            else:
-                # Compute the most common class from only the resulting data
-                prediction = self._get_majority_class(matching_data[:, -1])
-
-            predictions.append(prediction)
-
-        f1 = metrics.f1_score(valid_data[:, -1], predictions, average="weighted")
-
-        print(individual, f1)
-        return f1,
 
     def create_toolbox(self, pset):
         # Fitness is f1-score. So the larger the better
@@ -128,9 +93,6 @@ class EvolutionaryBase(Classifier):
 
             # Add the feature value nodes. These will form the children of the feature_node
             for value in feature_values:
-
-                print("Value is: ", value)
-
                 feature_output_name = feature_name+"_"+str(value) + "Type"
 
                 # Each feature value needs a special output type to ensure trees have a branch for each category
@@ -147,8 +109,6 @@ class EvolutionaryBase(Classifier):
             self.pset.addPrimitive(lambda *xargs, feature_idx=feature_index: self.feature_node(feature_idx, *xargs),
                                    [mask_type, *feature_node_input_types],
                                    train_data_type, name=feature_name)
-
-
 
     def fit(self, x, y):
         # Ensure we use numpy arrays
